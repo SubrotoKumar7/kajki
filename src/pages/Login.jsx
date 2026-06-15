c59f197475994e0b0e3a4ec3e21c17958b1b163f
Subroto Kumar <subrotokumar021@gmail.com>
2026-06-15 11:18:27 -0400

    login page responsive

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,16 +3,16 @@ import SocialAuth from "../components/SocialAuth";
 const Login = () => {
     return (
       <div className="flex justify-center items-center h-screen">
-        <div className="grid grid-rows-12 grid-cols-12 gap-1 items-center p-5 md:w-200 h-150 shadow-2xl shadow-gray-400">
+        <div className="grid grid-rows-12 grid-cols-12 gap-1 items-center p-5 md:w-200 md:h-150 shadow-2xl shadow-gray-400">
           {/* welcome message */}
-          <div className="col-span-6 row-span-8">
+          <div className="md:col-span-6 col-span-12 md:row-span-8 row-span-full">
             <h1 className="text-center text-3xl">
               Hello! <br /> <b>Have a good day</b>
             </h1>
           </div>
           {/* login form */}
-          <div className="col-span-6 row-span-8 p-2">
-            <h1 className="text-3xl text-center font-bold mb-3">Login</h1>
+          <div className="md:col-span-6 col-span-12 md:row-span-8 p-2 mt-3">
+            <h1 className="text-3xl text-center font-bold mb-3 md:block hidden">Login</h1>
             <fieldset className="fieldset">
               <label className="label">Email</label>
               <input
